fe7f1243320dfd97f594d3a3e93fe754ce0b66dd
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-01 12:04:34 +0500

    Add work.

---
+++ app.js
@@ -0,0 +1,31 @@
+// Question # 01
+var num1 = 3 ;
+var num2 = 5 ;
+var num3 = num1 + num2;
+
+document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
+
+
+var num1 = 3 ;
+var num2 = 5 ;
+var num3 = num1 - num2;
+
+document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
+
+var num1 = 3 ;
+var num2 = 5 ;
+var num3 = num1 * num2;
+
+document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
+
+var num1 = 3 ;
+var num2 = 5 ;
+var num3 = num1 / num2;
+
+document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
+
+var num1 = 3 ;
+var num2 = 5 ;
+var num3 = num1 % num2;
+
+document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
