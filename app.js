357d5de34872395ea3b1d2801b80d8690dafb30c
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-03 01:17:06 +0500

    add Work.

--- app.js
+++ app.js
@@ -1,31 +1,172 @@
 // Question # 01
-var num1 = 3 ;
-var num2 = 5 ;
-var num3 = num1 + num2;
+// var num1 = 3 ;
+// var num2 = 5 ;
+// var num3 = num1 + num2;
 
-document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
+// document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
 
 
-var num1 = 3 ;
-var num2 = 5 ;
-var num3 = num1 - num2;
+// Question # 02
+// var num1 = 3 ;
+// var num2 = 5 ;
+// var num3 = num1 - num2;
 
-document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
+// document.write ("Subraction of " + num1 + " - " + num2 + " is " + num3 + "</br>");
 
-var num1 = 3 ;
-var num2 = 5 ;
-var num3 = num1 * num2;
+// var num1 = 3 ;
+// var num2 = 5 ;
+// var num3 = num1 * num2;
 
-document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
+// document.write ("Multiply of " + num1 + " * " + num2 + " is " + num3 + "</br>");
 
-var num1 = 3 ;
-var num2 = 5 ;
-var num3 = num1 / num2;
+// var num1 = 3 ;
+// var num2 = 5 ;
+// var num3 = num1 / num2;
 
-document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
+// document.write ("Divide of " + num1 + " / " + num2 + " is " + num3 + "</br>");
 
 var num1 = 3 ;
 var num2 = 5 ;
 var num3 = num1 % num2;
 
-document.write ("Sum of " + num1 + " and " + num2 + " is " + num3 + "</br>");
+document.write ("Modulus of  " + num1 + " % " + num2 + " is " + num3 + "</br>");
+
+// Question # 03
+// Part a/b
+var x= "Value after variable declaration is ";
+var y= "undefined";
+document.write (x + y + "</br>");
+
+// Part c/d
+var a= 5;
+var b= 6;
+
+document.write ("Initial value:" + a + "</br>");
+
+// Part e/f 
+var c= 5;
+c++;
+document.write ("Value after increment is: " + c + "</br>");
+
+// Part g/h 
+var d= 6;
+document.write ("Value after addition is: " + (d+7) + "</br>");
+
+// Part i/j 
+var e= 19 ;
+document.write ("Value after decrement is: " + (e-7) + "</br>");
+
+// Part k/l 
+var num= 0;
+document.write ("The remainder is : " + num)
+
+
+// Question # 04
+// var cost = "600 ";
+// var ticket= "5";
+// var total = cost * ticket;
+
+// document.write ("Total cost to buy " + ticket + " tickets to a movie is " + total + " PKR");
+
+// Question # 05
+// let n = 4;
+// for (let i = 1; i <= 10; ++i)
+// 	document.write (n + " * " + i + " = " + n * i + "</br>");
+
+// Question # 06
+// var Celsius = 25;
+// var fahrenheit = 70;
+
+// document.write (Celsius + "°C" + " is " + ((Celsius * 9/5) + 32 + "°F" + "</br>"));
+
+// document.write (fahrenheit + "°F" + " is " + ((fahrenheit - 32) * 5/9 + "°C"));
+
+// Question # 07
+// document.write ("<h1> Shopping Cart </h1>")
+
+// var one= "Price of item 1";
+// var two= "Price of item 2";
+// var three= "Quantity of item 1";
+// var four= "Quantity of item 2";
+// var five= "Shipping charges";
+// var six= "Total cost of your order";
+
+// document.write (one + " is 650 </br>");
+// document.write (three + " is 3 </br>");
+// document.write (two + " is 100 </br>");
+// document.write (four + " is 7 </br>");
+// document.write (five + " 100 </br></br>");
+// document.write (six + " is 2750 ")
+
+
+// Question # 08
+// var TM= 980;
+// var OM= 804;
+// var percentage= OM / TM * 100;
+
+// document.write ("<h1>Marks Sheet</h1>");
+// document.write ("Total marks: " + TM + "</br>");
+// document.write ("Marks Obtained: " + OM + "</br>");
+// document.write ("Percentage: " + percentage + " %");
+
+// Question # 09
+// document.write ("<h1>Currency In PKR</h1>");
+// var dollar= 104.80 * 10;
+// var riyal= 28 * 25;
+// var PKR = dollar + riyal;
+
+// document.write ("Total Currency In PKR: " + PKR);
+
+// Question # 10
+// var x=12;
+// var y=13;
+// var z=16;
+
+// document.write ("Your Answer Is" + " " + ((x + y + z + 5) * 10) / 2);
+
+// Question # 11
+// document.write ("<h1>AGE CALCULATOR</h1>");
+// var CY= 2024;
+// var BY= 2005;
+// var Age= CY - BY;
+
+// document.write ("Current Year: " + CY + "</br>");
+// document.write ("Birth Year: " + BY + "</br>");
+// document.write ("Your Age is: " + Age);
+
+// Question # 12
+// document.write ("<h1>The Geometrizer</h1>");
+
+// var r= 20;
+// var π = 3.142;
+
+// document.write ("Radius of a circle: " + r + "</br>");
+// document.write ("The Circumference is: " + 2 * π * r + "</br>");
+// document.write ("The Area is: " + π * r * r);
+
+// Question # 13
+// document.write ("<h1>The Lifetime Supply Calculator:</h1>")
+
+// var favouriteSnacks= "Chocolate chip";
+// var Age= 15;
+// var Mage= 65;
+// var SPD= 3;
+// var total= Mage * SPD - 45;
+
+// document.write ("Favourite Snacks: " + favouriteSnacks + "</br>");
+// document.write ("Current age: " + Age + "</br>");
+// document.write ("Estimated Maximum Age: " + Mage + "</br>");
+// document.write ("Amount of snacks per day: " + SPD + "</br>");
+// document.write ("You will need " + total + " to last you until the ripe old age of " + Mage);
+
+
+
+
+
+
+
+
+
+
+
+
